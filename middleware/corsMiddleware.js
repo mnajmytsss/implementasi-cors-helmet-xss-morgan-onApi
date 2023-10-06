b6e6f7dd81afc36e99886bccd2dcd303e2049048
mnajmytsss <najmytsaqiby22@gmail.com>
2023-10-07 02:50:19 +0700

    update corsMiddleware

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,7 +1,29 @@
 const cors = require('cors')
 
+const origin = ["http://localhost:5173"];
+const partnerOrigin = ["http://localhost:5174"];
+
+const corsOptionsDelegate = (req, callback) => {
+    const clientOrigin = origin.includes(req.header("Origin"));
+    const clientPartnerOrigin = partnerOrigin.includes(req.header("Origin"))
+
+    if (clientOrigin) {
+        callback(null, {
+          origin: true,
+          methods: "GET, POST, DELETE, PUT, OPTIONS, HEAD",
+        });
+      } else if (clientPartnerOrigin) {
+        callback(null, {
+          origin: true,
+          methods: "GET, POST",
+        });
+      } else {
+        callback(new Error("Blocked by CORS"));
+      }
+};
+
 const corsMiddleware = (app) => {
-    app.use(cors());
+    app.use(cors(corsOptionsDelegate));
 };
 
 module.exports = corsMiddleware;
