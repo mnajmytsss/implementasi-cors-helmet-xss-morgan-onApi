9cac6f765220ed9dc624710e4a83dc48fc3bf4fc
mnajmytsss <135803765+mnajmytsss@users.noreply.github.com>
2023-10-17 17:01:38 +0700

    Add files via upload

---
+++ corsMiddleware.js
@@ -0,0 +1,35 @@
+const cors = require('cors')
+
+const origin = [
+  "https://week-15-mnajmytsss.vercel.app", 
+  "https://charming-erin-sunbonnet.cyclic.cloud/",
+];
+const partnerOrigin = [
+  "https://week15.avicena.dev",
+  "https://week15-defficharlina-fe.vercel.app" 
+];
+
+const corsOptionsDelegate = (req, callback) => {
+    const clientOrigin = origin.includes(req.header("Origin"));
+    const clientPartnerOrigin = partnerOrigin.includes(req.header("Origin"))
+
+    if (clientOrigin) {
+        callback(null, {
+          origin: true,
+          methods: "GET, POST, DELETE, PUT, OPTIONS, HEAD",
+        });
+      } else if (clientPartnerOrigin) {
+        callback(null, {
+          origin: true,
+          methods: "GET, POST",
+        });
+      } else {
+        callback(new Error("Blocked by CORS"));
+      }
+};
+
+const corsMiddleware = (app) => {
+    app.use(cors(corsOptionsDelegate));
+};
+
+module.exports = corsMiddleware;
